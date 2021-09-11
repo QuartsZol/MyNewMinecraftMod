006455ba5f37a1c1b90cec555db0f4eb45439fcf
QuartsZol <90087057+QuartsZol@users.noreply.github.com>
2021-09-11 20:34:00 +0930

    Update LoadingPage.lua

--- LoadingPage.lua
+++ LoadingPage.lua
@@ -1,29 +1,29 @@
 if game.PlaceId == 286090429 then -- Arsenal
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/Arsenal"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/Arsenal.lua"))();
 
 elseif game.PlaceId == 3956818381 then -- Ninja Legneds
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/NinjaLegends.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/NinjaLegends.lua"))();
 
 elseif game.PlaceId == 621129760 then --KAT
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/KAT"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/KAT.lua"))();
 
 elseif game.PlaceId == 648362523 then -- Beaking Point
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/Breaking%20Point"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/Breaking%20Point.lua"))();
 
 elseif game.PlaceId == 379614936 then -- Assasin
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/Assasin"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/Assasin.lua"))();
 elseif game.PlaceId == 5006801542 then -- Assasin Endless Mode
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/Assasin"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/Assasin.lua"))();
 
 
 elseif game.PlaceId == 3527629287 then -- BP
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/Big%20PaintBall"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/Big%20PaintBall.lua"))();
 
 elseif game.PlaceId == 983224898 then -- Wild Revolvers
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/Big%20PaintBall"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/Wild%20Revolvers.lua"))();
 
 elseif game.PlaceId == 1962086868 then -- TOH
-loadstring(game:HttpGet("https://raw.githubusercontent.com/F-uckOff/Fuck-Off-you-Bitch/main/Fuck%20Off%20Go%20Away/GamesAPI/TOH"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/QuartsZol/MyNewMinecraftMod/main/MinecraftBase/BaseAPI/TOH.lua"))();
 
 else
 warn("Your Game Isn't Supported! Please use the command ;games in our discord server to see the current commands. if there is an issue use the command ;ticket in #bot-commands")
